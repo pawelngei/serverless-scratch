368a0f77ef54982dd2ba48c790f55a02ad5f349a
alxd <github@alxd.org>
2019-07-03 10:33:59 +0200

    add AWS Amplify

---
+++ notes-app-client/src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "eu-west1-",
+    BUCKET: "alxd-lambda-scratch"
+  },
+  apiGateway: {
+    REGION: "eu-west-1",
+    URL: "https://6m6psuqepc.execute-api.eu-west-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "eu-west-1",
+    USER_POOL_ID: "eu-west-1_xUC4DsFWg",
+    APP_CLIENT_ID: "35nlqe0qqtk9srsjg2ej4jpq82",
+    IDENTITY_POOL_ID: "eu-west-1:fbdd591c-778d-445c-9db2-831dd2003336"
+  }
+};
